121e0a507573e49ad7632d906c84fcb0c6a24d23
bhoomi2716 <bhoomiwadhvana@gmail.com>
2025-01-23 15:44:06 +0530

    commit

---
+++ Task 3 BottomNavBar/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.assignment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment()
+{
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        var HomeFragment = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return HomeFragment
+    }
+}
